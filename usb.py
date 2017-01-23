cd79f6a05d1e80da1dd31886828d379854faf16d
anupamsingh610 <anupamsingh610@gmail.com>
2017-01-23 21:17:12 +0530

    file exists error

--- usb.py
+++ usb.py
@@ -9,6 +9,8 @@ def usbexport():
 	else:
 		source = "/home/pi/Desktop/"
 		destination = "/media/pi/CRTEXPORT"
+		if os.path.exists("/media/pi/CRTEXPORT/exporting.dat"):
+			os.remove("/media/pi/CRTEXPORT/exporting.dat")
 		#dest2="/media/pi/CRT/testing.text"
 	    #CHANGE NAME OF TANVI TO CRT AND THE PENDRIVE BEING CONNECTED SHOULD BE NAMED CRT
 		for filename in os.listdir(source):
