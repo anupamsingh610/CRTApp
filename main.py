f6fe97a01eae7d175ab2285971d82bbdf55ffbca
anupamsingh610 <anupamsingh610@gmail.com>
2017-01-23 17:57:37 +0530

    minor bug fixes

--- main.py
+++ main.py
@@ -1637,8 +1637,8 @@ class ViewOutput(tk.Frame):
 
 
         backButton = tk.Button(buttons, text = "Menu", command = lambda : controller.show_frame(Menu),image=controller.menu,compound="left",font=controller.buttonFont)
-        bButton = tk.Button(buttons,  command = lambda : self.backPage(controller,out,bButton,nButton),width = 3,state="disabled",image=controller.back,compound="left",font=controller.buttonFont)
-        nButton = tk.Button(buttons, command = lambda : self.nextPage(controller,out,bButton,nButton), width = 3,image=controller.next,compound="right",font=controller.buttonFont)
+        bButton = tk.Button(buttons,  command = lambda : self.backPage(controller,out,bButton,nButton),width = 20,state="disabled",image=controller.back,compound="left",font=controller.buttonFont)
+        nButton = tk.Button(buttons, command = lambda : self.nextPage(controller,out,bButton,nButton), width = 20,image=controller.next,compound="right",font=controller.buttonFont)
         # self.checkNextBack(controller,bButton,nButton)
 
         bButton.grid(padx = 5,row=0,column=0)
